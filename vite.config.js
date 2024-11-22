6c42fa4a1710d39cbbb850648cb928429955bb53
Kieran Kim-Murphy <kimmurkj@gmail.com>
2024-11-22 15:36:26 -0800

    "build: add server params to vite for development purposes"

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  server: {
+    open: true,
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
