7684cf79b2b61cdea4585837eecfff5b0fab6491
Kieran Kim-Murphy <kimmurkj@gmail.com>
2024-11-22 00:19:06 -0800

    feat: make react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MapDrawer from "./components/mapdrawer/MapDrawer";
+
+function App() {
+    return (
+        <div>
+            <MapDrawer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/mapdrawer/MapDrawer.jsx
@@ -0,0 +1,11 @@
+import Map from '../map/Map';
+
+function MapDrawer() {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
+
+export default MapDrawer;
