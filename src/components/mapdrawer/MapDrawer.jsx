c6f563285eecdd7c1127dd3a56a2d0ef308051dc
Kieran Kim-Murphy <kimmurkj@gmail.com>
2024-11-22 18:50:07 -0800

    fix: change map toggle button svg attributes to align with jsx

--- src/components/mapdrawer/MapDrawer.jsx
+++ src/components/mapdrawer/MapDrawer.jsx
@@ -20,9 +20,9 @@ function MapDrawer() {
           <path
             d="M9 20L3 17V4L9 7M9 20L15 17M9 20V7M15 17L21 20V7L15 4M15 17V4M9 7L15 4"
             stroke="#000"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </ToggleButton>
