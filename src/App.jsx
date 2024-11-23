69a149ad12d61656d0d9a7931a0e04d1de9b85f6
Kieran Kim-Murphy <kimmurkj@gmail.com>
2024-11-22 19:46:35 -0800

    wip: add toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import MapDrawer from "./components/mapdrawer/MapDrawer";
+import Toolbar from "./components/toolbar/Toolbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <MapDrawer />
+      <Toolbar />
     </div>
   );
 }
