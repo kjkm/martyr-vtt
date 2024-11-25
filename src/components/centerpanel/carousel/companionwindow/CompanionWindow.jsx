2b633508947bf8f87b55b72ac96b62ceaa1b8cd4
Kieran Kim-Murphy <kimmurkj@gmail.com>
2024-11-25 12:24:22 -0800

    wip: style companion window

--- src/components/centerpanel/carousel/companionwindow/CompanionWindow.jsx
+++ src/components/centerpanel/carousel/companionwindow/CompanionWindow.jsx
@@ -4,12 +4,8 @@ function CompanionWindow({ companions }) {
   return (
     <div className="CompanionWindow">
       {companions.map((companion, index) => (
-        <div key={index} className="CompanionCard">
-          <img
-            src={companion.thumbnail}
-            alt={`${companion.name}'s profile`}
-            className="CompanionCard-thumbnail"
-          />
+        <div key={index} className={`CompanionCard ${companion.name.toLowerCase()}`}>
+          <img src={companion.thumbnail} alt={`${companion.name}'s profile`} className="CompanionCard-thumbnail" />
           <div className="CompanionCard-info">
             <h3 className="CompanionCard-name">{companion.name}</h3>
             <p className="CompanionCard-stats">Body: {companion.body}</p>
