7c49484ff1756be9483c735ca7112d04b656514b
Kieran Kim-Murphy <kimmurkj@gmail.com>
2024-11-25 12:24:39 -0800

    wip: add default map image

--- src/components/mapdrawer/map/Map.jsx
+++ src/components/mapdrawer/map/Map.jsx
@@ -3,6 +3,7 @@ import "./Map.css";
 function Map() {
   return (
     <div className="Map">
+      <img src="../../../assets/default-map.webp" alt="Default Map" className="Map-image" />
       <h1>No map loaded.</h1>
     </div>
   );
