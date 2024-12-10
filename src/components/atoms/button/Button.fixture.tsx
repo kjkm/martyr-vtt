607b2effc3487cfdc121f3d3b1eed2e049d40d2b
Kieran Kim-Murphy <kimmurkj@gmail.com>
2024-12-10 10:19:22 -0800

    refactor: create atomic button component

---
+++ src/components/atoms/button/Button.fixture.tsx
@@ -0,0 +1,5 @@
+import Button from "./Button";
+
+export default (
+    <Button onClick={() => console.log("Button clicked")}>Click me</Button>
+);
---
+++ src/components/atoms/button/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Button.css";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return (
+    <button className="Button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
